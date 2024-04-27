3810129e041b16d555346ee2b0a6fd11446536d4
egomann88 <justin-urbanek@bluewin.ch>
2024-04-27 17:50:04 +0200

    feat: .env file has not predefined values for validation to detect empty strings

--- src/mange_env_service/manage_env.py
+++ src/mange_env_service/manage_env.py
@@ -16,12 +16,12 @@ def create_env_file(env_path):
     print("Creating .env file...")
     os.makedirs(os.path.dirname(env_path), exist_ok=True)
     with open(env_path, 'w') as f:
-        f.write("API_KEY=your_api_key\n")
-        f.write("EMAIL=your_email\n")
-        f.write("SUBDOMAIN=my")
-        f.write("START_STOP_TIMES=['08:00:00', '17:00:00']\n")
-        f.write("SERVICES_ID=your_services_id\n")
-        f.write("CUSTOMERS_ID=your_customers_id\n")
+        f.write("API_KEY=\n")
+        f.write("EMAIL=\n")
+        f.write("SUBDOMAIN=\n")
+        f.write("START_STOP_TIMES=\n")
+        f.write("SERVICES_ID=\n")
+        f.write("CUSTOMERS_ID=\n")
     print("The .env file has been created at:", env_path)
     print("Please fill in the required values.")
 
