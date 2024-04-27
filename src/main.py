be87b7e2725ae35d51950685180369af4c5e900a
egomann88 <justin-urbanek@bluewin.ch>
2024-04-27 19:52:01 +0200

    feat: global env_path in main.py

--- src/main.py
+++ src/main.py
@@ -1,25 +1,28 @@
 from mange_env_service.manage_env import check_env_file, check_env_correctness, incorrect_env_values, env_write_customer_service
+import os
+
+ENV_PATH = os.path.join("configs", ".env")
 
 def save_timer_id(timer_id):
     with open('tmp.txt', 'w') as f:
         f.write(timer_id)
 
 def fill_customers_services(value_matches):
-    # if API_KEY, EMAIL and SUBDOMAIN are True
+    # if API_KEY, EMAIL and SUBDOMAIN are True, while SERVICES_ID and CUSTOMERS_ID are False
     if value_matches[0] and value_matches[1] and value_matches[2] and not value_matches[4] and not value_matches[5]:
         user_input = input("Do you want to make a request to the Clockodo API to get the ID values of your services and customers? (y/n)")
         if user_input.lower() == 'y':
-            env_write_customer_service()
+            env_write_customer_service(ENV_PATH)
             print("Request to the Clockodo API made successfully.")
         return
 
 def main():
-    if not check_env_file():
+    if not check_env_file(ENV_PATH):
         print(".env file created. Please fill in the required values and run the script again.")
         print("If you don't know the ID values of your services and customers, enter API_KEY and EMAIL in the .env file and run the script again.")
         return
     
-    res = check_env_correctness()
+    res = check_env_correctness(ENV_PATH)
     
     # If any of the environment variables are missing or incorrect
     if not all(res):
