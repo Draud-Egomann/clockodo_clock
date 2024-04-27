526663d43a894dd40e4423bbd4de6853645367a2
egomann88 <justin-urbanek@bluewin.ch>
2024-04-27 15:24:25 +0200

    feat: incorrect values in env file checker

--- src/main.py
+++ src/main.py
@@ -1,11 +1,20 @@
-from mange_env_service.manage_env import check_env_file
+from mange_env_service.manage_env import check_env_file, check_env_correctness, incorrect_env_values
 
 def save_timer_id(timer_id):
     with open('tmp.txt', 'w') as f:
         f.write(timer_id)
 
 def main():
-    check_env_file()
+    if not check_env_file():
+        print(".env file created. Please fill in the required values and run the script again.")
+        print("If you don't know the ID values of your services and customers, enter API_KEY and EMAIL in the .env file and run the script again.")
+        return
+    
+    res = check_env_correctness()
+    
+    # If any of the environment variables are missing or incorrect
+    if not all(res):
+        incorrect_env_values(res)
 
     # Your main logic here
 
--- src/mange_env_service/manage_env.py
+++ src/mange_env_service/manage_env.py
@@ -1,11 +1,16 @@
 import os
+import re
+
+def env_values():
+    return ['API_KEY', 'EMAIL', 'START_STOP_TIMES', 'SERVICES_ID', 'CUSTOMERS_ID']
 
 def check_env_file():
     env_path = os.path.join('configs', '.env')
-    print(env_path)
     if not os.path.exists(env_path):
         print("No .env file for Clockodo API found")
         create_env_file(env_path)
+        return False
+    return True
 
 def create_env_file(env_path):
     print("Creating .env file...")
@@ -18,3 +23,47 @@ def create_env_file(env_path):
         f.write("CUSTOMERS_ID=your_customers_id\n")
     print("The .env file has been created at:", env_path)
     print("Please fill in the required values.")
+
+def check_env_correctness():
+    env_path = os.path.join('configs', '.env')
+    with open(env_path, 'r') as f:
+        env = f.read()
+
+        # Check if all environment variables are present
+        api_key_match = re.search(r'API_KEY=(\w+)', env)
+        email_match = re.search(r'EMAIL=(\w+)', env)
+        start_stop_times_match = re.search(r'START_STOP_TIMES=\[(\d{2}:\d{2}:\d{2}), (\d{2}:\d{2}:\d{2})\]', env)
+        services_id_match = re.search(r'SERVICES_ID=(\d+)', env)
+        customers_id_match = re.search(r'CUSTOMERS_ID=(\d+)', env)
+
+        env_variables = [api_key_match, email_match, start_stop_times_match, services_id_match, customers_id_match]
+        env_value_matches = []
+
+        for env_var in env_variables:
+            if not env_var:
+                env_value_matches.append(False)
+            else:
+                env_value_matches.append(True)
+        
+        return env_value_matches
+
+def incorrect_env_values(value_matches):
+    values = env_values()
+    print("Following values are missing or incorrect in the .env file:")
+    for i, value in enumerate(value_matches):
+        if not value:
+            print(f"-  {values[i]}")      
+    # if API_KEY and EMAIL are True
+    if value_matches[0] and value_matches[1]:
+        user_input = input("Do you want to make a request to the Clockodo API to get the ID values of your services and customers? (y/n)")
+        if user_input.lower() == 'y':
+            # get_ids_from_api()
+            print("Request to the Clockodo API made successfully.")
+        return
+    print("Please fill in the required values in the .env file and run the script again.")
+    return
+
+def get_ids_from_api():
+    # Your logic to make a request to the Clockodo API here
+    print("Request to the Clockodo API made successfully.")
+    return 'your_services_id=123456', 'your_customers_id=123456'
