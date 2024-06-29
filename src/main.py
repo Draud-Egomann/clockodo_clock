fd470daa2b422dba3bae9f8650415b941e701ba1
egomann88 <justin-urbanek@bluewin.ch>
2024-06-29 17:46:20 +0200

    fix: value_matches

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ ENV_PATH = os.path.join(os.path.dirname(__file__), ".env")
 
 def fill_customers_services(value_matches):
     # if API_KEY, EMAIL and SUBDOMAIN are True, while SERVICES_ID and CUSTOMERS_ID are False
-    if value_matches[0] and value_matches[1] and value_matches[2] and not value_matches[4] and not value_matches[5]:
+    if value_matches[0] and value_matches[1] and value_matches[2] and not value_matches[3] and not value_matches[4]:
         user_input = input("Do you want to make a request to the Clockodo API to get the ID values of your services and customers? (y/n)")
         if user_input.lower() == 'y':
             env_write_customer_service(ENV_PATH, "customers", "CUSTOMERS_ID")
